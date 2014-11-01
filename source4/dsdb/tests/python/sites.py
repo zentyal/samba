95dd39482af597e9960d1c14cb0defb19bb6d657
Jelmer Vernooij <jelmer@samba.org>
2014-11-01 10:28:23 -0700

    Add support for finding extras/mimeparse in old style Python tests.
    Eventually these tests should be converted to "real" Python unit tests,
    making these hacks unnecessary.
    
    Change-Id: Id760285a38250830dd7d4f948db602d00cabcb6f
    Signed-Off-By: Jelmer Vernooij <jelmer@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/dsdb/tests/python/sites.py
+++ source4/dsdb/tests/python/sites.py
@@ -22,6 +22,8 @@ import optparse
 import sys
 sys.path.insert(0, "bin/python")
 import samba
+samba.ensure_external_module("mimeparse", "mimeparse")
+samba.ensure_external_module("extras", "extras")
 samba.ensure_external_module("testtools", "testtools")
 samba.ensure_external_module("subunit", "subunit/python")
 
